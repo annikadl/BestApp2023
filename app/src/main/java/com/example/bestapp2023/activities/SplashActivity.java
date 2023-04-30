ef448813f312a905b399126b456689cabce560fb
Annika Delucchi <S4975984@studenti.unige.it>
2023-04-30 16:51:56 +0200

    ora parte con la Splash Activity, bastava mettere true nel manifest

--- app/src/main/java/com/example/bestapp2023/activities/SplashActivity.java
+++ app/src/main/java/com/example/bestapp2023/activities/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.bestapp2023.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,8 +11,10 @@ import java.util.Random;
 
 import com.example.bestapp2023.R;
 import com.example.bestapp2023.models.FirebaseWrapper;
+import com.example.bestapp2023.models.PermissionManager;
 
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     private static final String TAG = SplashActivity.class.getCanonicalName();
@@ -17,15 +22,48 @@ public class SplashActivity extends AppCompatActivity {
     // Only positive int
     private static final int PERMISSION_REQUEST_CODE = (new Random()).nextInt() & Integer.MAX_VALUE;
 
+    private void goToActivity(Class<?> activity) {
+        Intent intent = new Intent(this, activity);
+        this.startActivity(intent);
+        this.finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         // TODO: check user
-        // if not logged switch to login fragment
+        // copiato dal bro. TODO: Enter Activity che gestisce il login e la registrazione
+
+
+        // copiato anche questo dal bro.
+        // TODO: richiedere permessi e fare permission manager
+        // Check permissions -- Do not request at the login!
+
+        PermissionManager pm = new PermissionManager(this);
+        if (!pm.askNeededPermissions(PERMISSION_REQUEST_CODE, false)) {
+            // Go to MainActivity
+            this.goToActivity(MainActivity.class);
+        }
+
+        // Add the listener to the 'Grant Now' permission
+        this.findViewById(R.id.IntroButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!pm.askNeededPermissions(PERMISSION_REQUEST_CODE, true)) {
+                    // Go to MainActivity
+                    SplashActivity.this.goToActivity(MainActivity.class);
+                }
+            }
+        });
     }
 
     // TODO: onClick
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/example/bestapp2023/models/PermissionManager.java
@@ -0,0 +1,73 @@
+package com.example.bestapp2023.models;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermissionManager {
+    private final static String[] NEEDED_PERMISSIONS = {Manifest.permission.READ_CALENDAR};
+    private final static String TAG = PermissionManager.class.getCanonicalName();
+
+    private final Activity activity;
+
+    public PermissionManager(Activity activity) {
+        this.activity = activity;
+    }
+
+    public boolean askNeededPermissions(int requestCode, boolean performRequest) {
+        List<String> missingPermissions = new ArrayList<>();
+
+        for (String permission : NEEDED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this.activity, permission) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission " + permission + " is granted by the user.");
+            } /*
+            TODO: Considering showing somehow the permission rationale.
+                In this app, we display a very short message in the SplashActivity itself.
+            else if (shouldShowRequestPermissionRationale(...)){
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+                showInContextUI(...);
+            }*/ else {
+                missingPermissions.add(permission);
+            }
+        }
+
+        if (missingPermissions.isEmpty()) {
+            return false;
+        }
+
+        /*
+        NOTE:
+            After some `requestPermissions` calls, Android automatically answer no to the popup.
+            To avoid that, the apps usually ask for the permission on one time (first run) and then open the settings activity.
+            For instance: https://stackoverflow.com/questions/32822101/how-can-i-programmatically-open-the-permission-screen-for-a-specific-app-on-andr
+            ```
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", this.activity.getPackageName(), null);
+            intent.setData(uri);
+            this.activity.startActivity(intent);
+            ```
+         */
+
+        if (performRequest) {
+            Log.d(TAG, "Request for missing permissions " + missingPermissions);
+
+            // https://developer.android.com/reference/androidx/core/app/ActivityCompat#requestPermissions(android.app.Activity,java.lang.String[],int)
+            ActivityCompat.requestPermissions(this.activity, missingPermissions.toArray(new String[missingPermissions.size()]), requestCode);
+        }
+
+        return true;
+    }
+
+}
