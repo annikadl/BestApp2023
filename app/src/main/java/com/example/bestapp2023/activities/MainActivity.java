1d627e789624b2d03be8ba8c976c10d50bdf8832
Annika Delucchi <S4975984@studenti.unige.it>
2023-06-08 16:11:22 +0200

    layout prenotazioni

--- app/src/main/java/com/example/bestapp2023/activities/MainActivity.java
+++ app/src/main/java/com/example/bestapp2023/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Intent;
 
+
 import android.content.pm.PackageManager;
 import android.nfc.Tag;
 import android.os.Bundle;
